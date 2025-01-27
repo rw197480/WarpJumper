733d2ecfc238aa1a5605538f1d02c4bf810c07ea
CharlesT2 <ct207902@ebrstudents.org>
2025-01-27 08:46:53 -0600

    Add files via upload

---
+++ WarpJumper-main/warp-jumper/teleporting/portal.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+func openportal():
+	$AnimatedSprite2D.play("PortalOpen")
+	pass
+
+func portalstay():
+	$AnimatedSprite2D.play("PortalStay")
+	pass
+
+func closeportal():
+	$AnimatedSprite2D.play("PortalClose")
+	pass
